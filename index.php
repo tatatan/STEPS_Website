99dd94908cd6e610b4358e44b4109e9ab7e107f9
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2017-10-21 11:40:42 +0700

    initilize the boilerplate for STEPS website

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+  <title>INDEX PAGE</title>
+  <!--Start of Required tag for Bootstrap 4.0-->
+  <link rel="stylesheet" href="./css/bootstrap_4.0/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/main.css">
+  <script src="./js/jquery_3.2.1/jquery-3.2.1.slim.min.js"></script>
+  <script src="./js/popper_1.12.3/popper.min.js"></script>
+  <script src="./js/bootstrap_4.0/bootstrap.min.js"></script>
+  <!--End of Required tag for Bootstrap 4.0-->
+</head>
+<body>
+  <?php include("./navbar.html"); ?>
+  <main role="main">
+    <div class="container">
+      <div class="jumbotron">
+        <h1>You should try to learn Bootstrap 4.0!!</h1>
+        <p><a href="https://getbootstrap.com/" class="btn btn-lg btn-primary">Learn More!</a></p>
+      </div>
+    </div>
+  </main>
+
+</body>
+</html>
