f29fc1691906eb437f0f5baa4eecb018b1eb1d04
siripat1997 <violinist_pookan@hotmai.com>
2017-10-24 16:30:54 +0700

    1st commit

---
+++ finance_form/RequestForm.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title> STEPS : Finance Request Form </title>
+		<header><h2> Finance Request Form</h2></header>
+	</head>
+	<body>
+		<form action="" method="post">
+			ฝ่าย <input type=text name="field" placeholder="ฝ่ายของคุณ">
+
+		</form>
+	</body>
+</html>
+
+
