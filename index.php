e48c8d2c46aaecc7b9f066048843dea21f535270
tatatan <32958443+tatatan@users.noreply.github.com>
2017-10-30 22:18:59 +0700

    hello

--- index.php
+++ index.php
@@ -1,24 +1,54 @@
 <!DOCTYPE html>
-<head>
-  <title>INDEX PAGE</title>
-  <!--Start of Required tag for Bootstrap 4.0-->
-  <link rel="stylesheet" href="./css/bootstrap_4.0/bootstrap.min.css">
-  <link rel="stylesheet" href="./css/main.css">
-  <script src="./js/jquery_3.2.1/jquery-3.2.1.slim.min.js"></script>
-  <script src="./js/popper_1.12.3/popper.min.js"></script>
-  <script src="./js/bootstrap_4.0/bootstrap.min.js"></script>
-  <!--End of Required tag for Bootstrap 4.0-->
-</head>
-<body>
-  <?php include("./navbar.html"); ?>
-  <main role="main">
-    <div class="container">
-      <div class="jumbotron">
-        <h1>You should try to learn Bootstrap 4.0!</h1>
-        <p><a href="https://getbootstrap.com/" class="btn btn-lg btn-primary">Learn More!</a></p>
-      </div>
+<?php 
+session_start();
+?>
+<html lang"en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+<nav class="navbar ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#">Steps</a>
     </div>
-  </main>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li> 
+        <li><a href="#">Page 3</a></li> 
 
-</body>
+        <!-- link for only-member page /-->
+         <?php
+         if((isset($_SESSION['fbid']))and(isset($_SESSION['nickname']))){
+           echo "<li><a href=\"#\">Page 4</a></li>";
+           echo "li><a href=\"#\">Page 5</a><li>";
+         }
+         ?>
+         <!-- link for fuction login/logout and memberpage /-->
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      <?php
+      if((isset($_SESSION['facebook_id']))and(isset($_SESSION['nickname']))){          
+          echo " <li><a href=\"member.php\"><span class=\"glyphicon glyphicon-user\"></span>".$_SESSION['nickname']."    </a></li>
+        <li><a href=\"logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span> Logout</a></li>";
+      }
+      else{
+        echo "<li><a href=\"regis.php\"><span class=\"glyphicon glyphicon-user\"></span> Sign Up</a></li>
+        <li><a href=\"login.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Login</a></li>";
+      } ?>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 </html>
