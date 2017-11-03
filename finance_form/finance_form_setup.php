2d8ae5dc8d0e9f3d8a4067352ccd5aa896cea9ce
siripat1997 <violinist_pookan@hotmai.com>
2017-11-03 18:41:38 +0700

    add table setup.

---
+++ finance_form/finance_form_setup.php
@@ -0,0 +1,24 @@
+<?php
+	$cmd="CREATE TABLE if not exists financerequests( 
+			FinanceRequestID INT AUTO_INCREMENT PRIMARY KEY,
+			Field TEXT NOT NULL,
+			Proposer TEXT NOT NULL,
+			PhoneNumber TEXT NOT NULL,
+			Project TEXT NOT NULL,
+			Approvement BOOLEAN NOT NULL,
+			Status TEXT NOT NULL,
+			Comment TEXT NOT NULL
+			)";
+	$cmd2="CREATE TABLE if not exists financedetails( 
+			Detail TEXT NOT NULL,
+			Quantity TEXT NOT NULL,
+			PricePerUnit TEXT NOT NULL,
+			FinanceRequestID INT,
+			FOREIGN KEY (FinanceRequestID) REFERENCES financerequests(FinanceRequestID)
+		)";
+
+	include("connect.php");
+	mysqli_query($conn,$cmd);
+ 	mysqli_query($conn,$cmd2);
+ 	$conn->close();		
+?>
